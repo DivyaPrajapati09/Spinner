f068440212fd79a0e096f94405c59c451a682a61
Divya Prajapati <divya4337@gmail.com>
2016-06-12 14:16:14 +0530

    final commit or the spinner

--- app/src/main/java/com/diya/spinner/MainActivity.java
+++ app/src/main/java/com/diya/spinner/MainActivity.java
@@ -2,9 +2,12 @@ package com.diya.spinner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Defining the spinner
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selection=String.valueOf(parent.getItemAtPosition(position));
+        Toast.makeText(MainActivity.this, "Selection is : "+selection, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
